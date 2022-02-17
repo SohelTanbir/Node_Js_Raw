279fd415e5419d11aa6bdf2662e0b2cfdb8ca6b5
SohelTanbir <soheltanbir19@gmail.com>
2022-02-17 16:42:25 +0600

    learned fs and url module with file create, read, update and delete

--- node-practice.js
+++ node-practice.js
@@ -2,9 +2,11 @@
 const http = require("http");
 const url =  require("url");
 const fs =  require("fs")
+const upperCase = require("upper-case")
+
+
 // custom modules
 const date = require("./_myModules/custom-module");
-const { throws } = require("assert");
 
 // port 
 const port = 5000;
@@ -13,9 +15,6 @@ const port = 5000;
 
 // create server
 const server = http.createServer(function(req, res){
-    res.write("Server is running...........")
-    // res.writeHead(200, {'content-type':'text/html'});
-
     // http and url module
     // const query = url.parse(req.url, true).query;
     // const name = query.name;
@@ -26,9 +25,9 @@ const server = http.createServer(function(req, res){
 
     // read file
     // fs.readFile("./file/fs.html", function(err, data){
+    //     console.log('error = '+ err);
     //     res.writeHead(200,{'Content-Type':'text/html'});
     //     res.write(data);
-    //     console.log('error = '+ err);
     //     return res.end()
     // })
 
@@ -41,7 +40,60 @@ const server = http.createServer(function(req, res){
     // })
    
     // create a empty file
-    fs.
+    // fs.open('./file/text1.txt', 'w', function(err, file){
+    //     if(err) throw err;
+    //     console.log("saved!");
+    //     res.end();
+    // })
+
+    // create a file if file already exist it will be override
+    // fs.writeFile("./file/newText.txt", "This is new replace content", function(err){
+    //     if(err) throw err;
+    //     console.log("File write and created!");
+    // })
+
+    // delete file
+    // fs.unlink("./file/text1.txt", (err)=>{
+    //     if(err) throw err;
+    //     console.log("Deleted Success");
+    // })
+
+    // rename file
+//   fs.rename("./file/demo.txt", './file/rename.txt', function(err){
+//       if(err) throw err;
+//       console.log("renamed success");
+//   })
+
+// file module end here
+
+
+// // url module start
+const paths = url.parse(req.url, true);
+// console.log(paths.search);
+// console.log('pathname = ' + paths.pathname);
+const pathname = paths.pathname;
+if(pathname == '/login'){
+    fs.readFile("./file/login.html", function(err, data){
+        if(err) throw err;
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    })
+}
+if(pathname == '/signup'){
+    fs.readFile("./file/signup.html", function(err, data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        if(err) throw err;
+        res.write(data);
+        res.end()
+    })
+}
+
+res.write(upperCase.upperCase("Welcome to npm install modules"));
+res.end()
+
+// url module end
+
 
 });
 
